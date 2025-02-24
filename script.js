10afe171b45c4e2c84a69a7263233ecd1018918e
olatona-enoch <enocholatona@gmail.com>
2025-02-24 07:55:09 +0100

    added to the javascript and style for the ticket page

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
                 if (!form.checkValidity()) {
                     event.preventDefault();
                     event.stopPropagation();
+                } else {
+                    event.preventDefault()
+                    generateTicket();// to call the ticket generator function
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -41,7 +44,7 @@ function uploadImage() {
             uploadBox.classList.add('is-invalid');
             fileError.style.display = 'block';
             imageView.innerHTML = '';  // Clear any previous image preview
-            imageView.innerHTML = `<img src="assets/images/icon-upload.svg" alt="Upload Icon" class="rounded bg-secondary bg-opacity-50 border border-1 p-2 w-25" >`; // Show upload icon again
+            imageView.innerHTML = `<img src="assets/images/icon-upload.svg" alt="Upload Icon" class="rounded bg-secondary bg-opacity-50 border border-1 p-2" >`; // Show upload icon again
         } else {
             // Reset error state for valid file
             uploadBox.classList.remove('is-invalid');
@@ -92,5 +95,58 @@ uploadBox.addEventListener('drop', (e) => {
     fileInput.files = e.dataTransfer.files;
     uploadImage();
 });
-// Optional: If you want the upload box to be clickable to trigger the file input
-// dropArea.addEventListener('click', () => fileInput.click());
+
+function generateTicket() {
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const githubUsername = document.getElementById("github").value;
+    const avatarImage = imageView.querySelector("img").src;
+    const ticketpage = document.getElementById("ticketpage");
+
+
+    ticketpage.innerHTML = `
+            <header class="text-center m-5">
+            <img src="assets/images/logo-full.svg" alt=""class="w-100-sm">
+        </header>
+        <div class="text-white text-center mb-3" id="info">
+            <p class="display-1">Congrats,<span>${fullName}!</span> Your ticket is ready.</p>
+            <p class="h5 mb-5">  We've emailed your ticket to <span>${email}</span> and will send updates in the run up to the event.</p>
+  
+        </div>
+
+        <div class="container justify-content-center  m-6 p-3 ticketbox rounded-4">
+            <div class=" d-flex justify-content-end text-white">
+              <div class="col-10 ps-2">
+                <div class="d-flex align-items-start">
+                  <div class="mt-2">           
+                    <img src="assets/images/logo-mark.svg" alt="" class="image-logo">
+                  </div>
+                  <div class="text-start ms-3 ">
+                    <h1>Coding Conf</h1>
+                    <p class="text-white-50">Jan 31, 2025/ Austin, TX</p>
+                  </div>
+                </div>
+                <div class="col d-flex align-items-center" >
+                  <img src=${avatarImage} alt="" class="ticket-image border border-1">
+                  <p class= "ps-3">${fullName}<br>
+                  <span class="small text-white-50"><img src="assets/images/icon-github.svg" alt="">${githubUsername}</span></p>
+                </div>
+              </div>
+              <div class="pe-2 cutline"></div>
+              <div class="col-2 upsidedown position-relative  ">
+                <p class="display-6 position-absolute top-50 start-50 translate-middle">#04045</p>
+              </div>
+            </div>
+
+    `
+    // window.location.href = "ticket.html";
+}
+// .ticketbox{
+//     background-color:rgba(255, 255, 255, 0.148) ;
+//     justify-self: center;
+//     max-width: 600px;
+//     overflow-x: hidden;
+//     border-width: 56px;
+//     border-radius: 20px;
+//     border: 2px solid #d55a5a; 
+// }
